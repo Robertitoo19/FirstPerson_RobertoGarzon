3d65a45b444b3f55015512d2769ad9a60db4d965
Robertitoo19 <robertito191103@gmail.com>
2024-11-23 01:00:25 +0100

    Flying

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,7 +5,7 @@ using UnityEngine.UIElements;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] GameObject enemyPrefabs;
+    [SerializeField] GameObject[] enemyPrefabs;
     [SerializeField] private Transform[] spawnPoints;
     [SerializeField] private int levels;
     [SerializeField] private int roundsxLevel;
@@ -29,7 +29,7 @@ public class Spawner : MonoBehaviour
             {
                 for (int k = 0; k < spawnxRound; k++)
                 {
-                    Instantiate(enemyPrefabs, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+                    Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Length)], spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
 
                     yield return new WaitForSeconds(waitSpawns);
                 }

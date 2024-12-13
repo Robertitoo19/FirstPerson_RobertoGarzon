cf155a6862618c2d4b63513ab01b62a7c96bd227
Robertitoo19 <robertito191103@gmail.com>
2024-12-13 01:39:00 +0100

    rdcd

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] Canvas pauseMenu;
+    bool pause;
+    void Start()
+    {
+        pauseMenu.gameObject.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            Pausar();
+        }
+    }
+    private void Pausar()
+    {
+        pause = !pause;
+        pauseMenu.gameObject.SetActive(pause);
+        Cursor.lockState = CursorLockMode.None;
+
+        if (pause == true)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+    public void Continue()
+    {
+        pauseMenu.gameObject.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
